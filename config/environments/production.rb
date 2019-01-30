b7da3391798c36124f7e83671edf36063874a1be
Nell Nayeong Park <napark8@gmail.com>
2019-01-30 16:16:36 -0500

    updated config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,9 +27,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.debug = true
-  config.assets.digest = true
+  #config.assets.compile = false
+  #config.assets.debug = true
+  #config.assets.digest = true
 
   config.public_file_server.enabled = true
 
