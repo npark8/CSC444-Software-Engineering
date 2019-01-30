819e5d7212f1961ea6bc1e25b111d49ac5586057
Nell Nayeong Park <napark8@gmail.com>
2019-01-30 16:06:30 -0500

    updated config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.debug = true
   config.assets.digest = true
 
@@ -68,7 +68,7 @@ Rails.application.configure do
     # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'acupofsugar.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'acupofsugar2.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
