e232dc4857a66ae2cb4a1d48e80d8fefaf685e1c
Nell Nayeong Park <napark8@gmail.com>
2019-01-30 16:11:39 -0500

    updated config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.digest = true
 
+  config.public_file_server.enabled = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
